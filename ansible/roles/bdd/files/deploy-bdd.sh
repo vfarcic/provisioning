c4e26b1a10aea11d9c23180c394a28a79ee27d3d
Viktor Farcic <viktor@farcic.com>
2014-12-27 20:50:36 +0100

    Added tests

--- ansible/roles/bdd/files/deploy-bdd.sh
+++ ansible/roles/bdd/files/deploy-bdd.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+echo "Setting variables and deciding whether to do blue or green deployment"
 BLUE_PORT=9001
 GREEN_PORT=9002
 CURRENT_COLOR=$(etcdctl get /bdd/color)
@@ -14,27 +15,32 @@ else
   COLOR="blue"
 fi
 
-echo "Starting $COLOR deployment on port $PORT"
+echo "Stopping and removing existing containers"
 set +e
 docker stop bdd-$COLOR
 docker rm bdd-$COLOR
 docker stop bdd-runner-phantomjs
 docker rm bdd-runner-phantomjs
+
+echo "Starting $COLOR deployment on port $PORT"
 set -e
 docker pull vfarcic/bdd
 docker run -d --name bdd-$COLOR -p $PORT:9000 vfarcic/bdd
-sleep 5
+
 echo "Testing $COLOR deployment on port $PORT"
 docker pull vfarcic/bdd-runner-phantomjs
 docker run -t --rm --name bdd-runner-phantomjs vfarcic/bdd-runner-phantomjs \
   --story_path data/stories/tcbdd/stories/storyEditorForm.story \
   --composites_path /opt/bdd/composites/TcBddComposites.groovy \
   -P url=http://172.17.42.1:$PORT -P widthHeight=1024,768
+
+echo "Storing new release information and update nginx"
 etcdctl set /bdd/color $COLOR
 etcdctl set /bdd/port $PORT
 etcdctl set /bdd/$COLOR/port $PORT
 etcdctl set /bdd/$COLOR/status running
 confd -onetime -backend etcd -node 127.0.0.1:4001
+
 echo "Stopping $CURRENT_COLOR deployment"
 sleep 10
 docker stop bdd-$CURRENT_COLOR
