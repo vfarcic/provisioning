424a185f0889e51ede47394b67ebd3f89ad92119
Viktor Farcic <viktor@farcic.com>
2015-01-11 01:36:21 +0100

    Started blue-green deployment role in bdd2

--- ansible/bootstrap.sh
+++ ansible/bootstrap.sh
@@ -6,4 +6,7 @@ git clone http://github.com/ansible/ansible
 source ansible/hacking/env-setup
 chmod +x ansible/hacking/test-module
 
-ansible/hacking/test-module -m libraries/bgdeployment.py -a "name=bdd blue_port=9001 green_port=9002"
+ansible/hacking/test-module -m library/bgdeployment.py -a "name=bdd blue_port=9001 green_port=9002 state=retrieved"
+ansible/hacking/test-module -m library/bgdeployment.py -a "name=bdd blue_port=9001 green_port=9002 state=changed"
+
+ansible-playbook bdd2.yml
